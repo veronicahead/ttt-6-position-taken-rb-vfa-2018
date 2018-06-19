940168bf40ad7f6eb8f782d707607b88c14ec1e2
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-19 19:23:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || ""
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
     return false
   end
 
