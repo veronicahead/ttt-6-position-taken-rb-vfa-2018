16a9168051e2ad5e39d52eb3320a0f061476d588
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-19 19:13:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || ""
     return false
-  elsif board[index] == "x" || "o"
+  elsif board[index] == "X" || "O"
     return true
   end
 end
